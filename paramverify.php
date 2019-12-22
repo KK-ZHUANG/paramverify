4c5466fbf9185912da12aafd4afda6c6f4f6ca75
kk <kkandmore@163.com>
2019-12-22 13:10:26 +0800

    v1.2.0

--- paramverify.php
+++ paramverify.php
@@ -4,7 +4,7 @@
  *
  * Parameter verify tool
  *
- * version 1.1.1
+ * version 1.2.0
  *
  * Author : KK (kkandmore@163.com)
  *
@@ -15,8 +15,17 @@ class Paramverify
    * The array of configuration
    */
   private static $config = [
+
     'required' => true,
+
     'type' => 'string',
+
+    're' => null,
+
+    'msg' => null,
+
+    'map' => [],
+
   ];
 
   /**
@@ -27,25 +36,33 @@ class Paramverify
   public static function config(array $config)
   {
     $verify = [
-      'required' => ['required' => false, 'type' => 'bool'],
-      'type' => ['required' => false, 'type' => 'string', 're' => '/^(int|float|string|numeric|bool)$/', 'msg' => 'The value of configuration parameter [type] must be int|float|string|numeric|bool'],
+      'required' => [
+        'required' => false,
+        'type' => 'bool'
+      ],
+      'type' => [
+        'required' => false,
+        'type' => 'string',
+        're' => '/^(int|float|string|numeric|bool)$/',
+        'msg' => 'The value of configuration parameter [type] must be int|float|string|numeric|bool'
+      ],
+      're' => [
+        'required' => false,
+        'type' => 'string'
+      ],
+      'msg' => [
+        'required' => false,
+        'type' => 'string'
+      ],
     ];
 
     $return = self::verify($config, $verify);
     
-    if(!$return['status'])
+    $return['status'] ? '' : exit($return['msg']);
+
+    foreach (self::$config as $k => $v)
     {
-      exit($return['msg']);
-    }
-    else
-    {
-      foreach (self::$config as $k => $v)
-      {
-        if(isset($config[$k]))
-        {
-          self::$config[$k] = $config[$k];
-        }
-      }
+      self::$config[$k] = $config[$k] ?? self::$config[$k];
     }
   }
 
@@ -60,54 +77,93 @@ class Paramverify
   {
     foreach($verify as $k => $v)
     {
+      foreach (self::$config as $config_index => $config_val)
+      {
+        $exec_config[$config_index] = $v[$config_index] ?? self::$config[$config_index];
+      }
+
       $index = explode('.', $k);
-      $param_ele = $param;
-      
+      $param_ele = &$param;
+
       foreach($index as $index_v)
       {
-        $required = isset($v['required']) ? $v['required'] : self::$config['required'];
-
-        if($required && !isset($param_ele[$index_v]))
+        if($exec_config['required'] && !isset($param_ele[$index_v]))
         {
-          $return  = ['status' => false, 'msg' => '['.$k.'] error code [1]'];
-          return $return;
+          return [
+            'status' => false,
+            'msg' => 'Error Code [1],['.$k.'] is required.'
+          ];
         }
         else
         {
-          @$param_ele = $param_ele[$index_v];
-        }
-      }
-      
-      if(isset($param_ele))
-      {
-        @$fun_call_name = 'is_'.($v['type'] ? $v['type'] : self::$config['type']);
-        if(!$fun_call_name($param_ele))
-        {
-          $return  = ['status' => false, 'msg' => '['.$k.'] error code [2]'];
-          return $return;
-        }
-        
-        if(isset($v['re']))
-        {
-          if((isset($v['type']) ? $v['type'] : self::$config['type']) === 'bool')
+          if(isset($param_ele[$index_v]))
           {
-            if(($v['re'] === 'true' && $param_ele !== true)||($v['re'] === 'false' && $param_ele !== false))
-            {
-              $return = ['status' => false,'msg' => isset($v['msg'])? $v['msg'] : '['.$k.'] error code [3]'];
-              return $return;
-            }
+            $param_ele = &$param_ele[$index_v];
           }
           else
           {
-            if(!preg_match($v['re'], $param_ele))
-            {
-              $return = ['status' => false,'msg' => isset($v['msg'])? $v['msg'] : '['.$k.'] error code [3]'];
-              return $return;
-            }
+            unset($param_ele);
+            $param_ele = null;
           }
         }
       }
+
+      if($param_ele !== null)
+      {
+        if($exec_config['map'])
+        {
+          foreach($exec_config['map'] as $fun_name)
+          {
+            $param_ele = $fun_name($param_ele);
+          }
+        }
+
+        $return  = self::type($k, $exec_config, $param_ele) ?? self::re($k, $exec_config, $param_ele);
+        if($return)
+        {
+          return $return;
+        }
+      }
     }
-    return ['status' => true, 'msg' => ''];
+    return [
+      'status' => true,
+      'msg' => $param,
+    ];
+  }
+
+  private static function type($index, $rule, $val)
+  {
+    $fun_call_name = 'is_'.$rule['type'];
+    if(!$fun_call_name($val))
+    {
+      return [
+        'status' => false,
+        'msg' => 'Error Code [2],['.$index.'] must be of the type '.$rule['type'].','.gettype($val).' given.',
+      ];
+    }
+  }
+
+  private static function re($index, $rule, $val)
+  {
+    if($rule['type'] === 'bool')
+    {
+      if(($rule['re'] === 'true' && $val !== true) || ($rule['re'] === 'false' && $val !== false))
+      {
+        return [
+          'status' => false,
+          'msg' => $rule['msg'] ?? 'Error Code [3],['.$index.'] re mismatch.',
+        ];
+      }
+    }
+    else
+    {
+      if(!preg_match($rule['re'], $val))
+      {
+        return [
+          'status' => false,
+          'msg' => $rule['msg'] ?? 'Error Code [3],['.$index.'] re mismatch.',
+        ];
+      }
+    }
   }
 }
